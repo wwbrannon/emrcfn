cbbf560d060a2d838c9cef547becffef7ea2bffa
William Brannon <will.brannon@gmail.com>
2018-06-21 20:03:21 -0400

    fix install script

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -5,6 +5,8 @@ set -e
 SOURCE="s3://cortico-public/hadoop/"
 
 aws s3 cp --recursive "$SOURCE" .
+
+hadoop fs -rm -r -f hdfs:/spatial/
 hadoop fs -mkdir hdfs:/spatial/
 hadoop fs -copyFromLocal jar/gis-tools-hadoop-2.0.jar hdfs:/spatial/
 hive -f sql/spatial.sql
