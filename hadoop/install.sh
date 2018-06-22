0af6e8226d1dc580f5add251ae2b8f582c150106
William Brannon <will.brannon@gmail.com>
2018-06-21 22:43:07 -0400

    refactor s3 prefix for geo components

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-SOURCE="s3://cortico-public/hadoop/"
-
-aws s3 cp --recursive "$SOURCE" .
+aws s3 cp --recursive "$1/hadoop/" .
 
 hadoop fs -rm -r -f hdfs:/spatial/
 hadoop fs -mkdir hdfs:/spatial/
 hadoop fs -copyFromLocal jar/gis-tools-hadoop-2.0.jar hdfs:/spatial/
+
 hive -f sql/spatial.sql
 
