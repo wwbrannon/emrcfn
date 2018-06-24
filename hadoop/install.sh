14e59876459bda1158915b4afd66c76dcefb1d26
William Brannon <will.brannon@gmail.com>
2018-06-24 11:25:43 -0400

    bing tiles

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -7,6 +7,8 @@ aws s3 cp --recursive "s3://$1/hadoop/" .
 hadoop fs -rm -r -f hdfs:/spatial/
 hadoop fs -mkdir hdfs:/spatial/
 hadoop fs -copyFromLocal jar/gis-tools-hadoop-2.0.jar hdfs:/spatial/
+hadoop fs -copyFromLocal jar/bing-tile-hive-1.0.jar hdfs:/spatial/
 
 hive -f sql/spatial.sql
+hive -f sql/bt.sql
 
