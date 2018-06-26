c4039b6a0713ba4ded2b3f1858d290f320eac0f5
William Brannon <will.brannon@gmail.com>
2018-06-25 23:08:25 -0400

    consolidate sql files

--- hadoop/sql/spatial.sql
+++ hadoop/sql/spatial.sql
@@ -82,3 +82,15 @@ drop function if exists ST_X; create function ST_X as 'com.esri.hadoop.hive.ST_X
 drop function if exists ST_Y; create function ST_Y as 'com.esri.hadoop.hive.ST_Y' using jar 'hdfs:/spatial/gis-tools-hadoop-2.0.jar';
 drop function if exists ST_Z; create function ST_Z as 'com.esri.hadoop.hive.ST_Z' using jar 'hdfs:/spatial/gis-tools-hadoop-2.0.jar';
 
+drop function if exists BT_AsGeometry; create function BT_AsGeometry as 'com.wwbrannon.bing.BT_AsGeometry' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_Equals; create function BT_Equals as 'com.wwbrannon.bing.BT_Equals' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_FromCoordinates; create function BT_FromCoordinates as 'com.wwbrannon.bing.BT_FromCoordinates' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_FromLatLon; create function BT_FromLatLon as 'com.wwbrannon.bing.BT_FromLatLon' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_FromQuadKey; create function BT_FromQuadKey as 'com.wwbrannon.bing.BT_FromQuadKey' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_GetQuadKey; create function BT_GetQuadKey as 'com.wwbrannon.bing.BT_GetQuadKey' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_GetX; create function BT_GetX as 'com.wwbrannon.bing.BT_GetX' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_GetY; create function BT_GetY as 'com.wwbrannon.bing.BT_GetY' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_GetZoomLevel; create function BT_GetZoomLevel as 'com.wwbrannon.bing.BT_GetZoomLevel' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_TilesAround; create function BT_TilesAround as 'com.wwbrannon.bing.BT_TilesAround' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+drop function if exists BT_TilesCoveringGeometry; create function BT_TilesCoveringGeometry as 'com.wwbrannon.bing.BT_TilesCoveringGeometry' using jar 'hdfs:/spatial/bing-tile-hive-1.0.jar';
+
