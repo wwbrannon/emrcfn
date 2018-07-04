a7cef99224cea4a7b53ba62761c97cc5f7873780
William Brannon <will.brannon@gmail.com>
2018-07-04 14:23:33 -0400

    update jupyterhub init step

--- steps/jupyterhub.sh
+++ steps/jupyterhub.sh
@@ -1,36 +1,24 @@
 #!/bin/bash -xe
 
-sudo docker exec jupyterhub << "EOF"
-#!/bin/bash -xe
+sudo docker exec -i jupyterhub /bin/bash << "EOF"
+set -xe
 
 apt-get update
 apt-get install build-essential
 
-INSTALL_DIR="/opt/conda"
-CONDA="$INSTALL_DIR/bin/conda"
-ACTIVATE="$INSTALL_DIR/bin/activate"
-DEACTIVATE="$INSTALL_DIR/bin/deactivate"
+CONDA="/opt/conda/bin/conda"
 
-"$CONDA" config --add channels conda-forge
-"$CONDA" config --add channels pytorch
-
-"$CONDA" create -y -n spark anaconda ipykernel python=3.6
-
-source "$ACTIVATE" spark
-python -m ipykernel install
-source "$DEACTIVATE"
-
-"$CONDA" install -y -n spark tensorflow theano keras pytorch torchvision \
-                             numpy pandas scipy blaze patsy \
-                             networkx nltk scikit-learn statsmodels \
-                             matplotlib seaborn bokeh \
-                             ipython nbconvert nbformat \
-                             beautifulsoup4 boto boto3 requests \
-                             xlrd xlsxwriter xlwt \
-                             psycopg2 pyodbc sqlalchemy \
-                             six us tqdm sqlalchemy pandoc pandocfilters \
-                             nose sympy snappy cython jinja2 lxml pyyaml \
-                             pyspark
+"$CONDA" install -y python=3.6
+"$CONDA" install -y tensorflow theano keras pytorch torchvision \
+                    numpy pandas scipy blaze patsy \
+                    networkx nltk scikit-learn statsmodels \
+                    matplotlib seaborn bokeh \
+                    ipython nbconvert nbformat \
+                    beautifulsoup4 boto boto3 requests \
+                    xlrd xlsxwriter xlwt \
+                    psycopg2 pyodbc sqlalchemy \
+                    six us tqdm sqlalchemy pandoc pandocfilters \
+                    nose sympy snappy cython jinja2 lxml pyyaml
 
 EOF
 
