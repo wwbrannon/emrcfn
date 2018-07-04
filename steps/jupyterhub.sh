10deed0b9232406cd87e914e6a03c2240dcff2fb
William Brannon <will.brannon@gmail.com>
2018-07-04 01:17:46 -0400

    jupyterhub

--- steps/jupyterhub.sh
+++ steps/jupyterhub.sh
@@ -1,18 +1,34 @@
 #!/bin/bash -xe
 
-conda config --add channels conda-forge
-conda config --add channels pytorch
+sudo docker exec jupyterhub << "EOF"
+apt-get update
+apt-get install build-essential
 
-/opt/anaconda/bin/conda install -y \
-                                   tensorflow theano keras pytorch torchvision \
-                                   numpy pandas scipy blaze patsy \
-                                   networkx nltk scikit-learn statsmodels \
-                                   matplotlib seaborn bokeh \
-                                   ipython nbconvert nbformat \
-                                   beautifulsoup4 boto boto3 requests \
-                                   xlrd xlsxwriter xlwt \
-                                   psycopg2 pyodbc sqlalchemy \
-                                   six us tqdm sqlalchemy pandoc pandocfilters \
-                                   nose sympy snappy cython jinja2 lxml pyyaml \
-                                   pyspark
+INSTALL_DIR="/opt/conda"
+CONDA="$INSTALL_DIR/bin/conda"
+ACTIVATE="$INSTALL_DIR/bin/activate"
+DEACTIVATE="$INSTALL_DIR/bin/deactivate"
+
+"$CONDA" config --add channels conda-forge
+"$CONDA" config --add channels pytorch
+
+"$CONDA" create -n spark anaconda ipykernel python=3.6
+source "$ACTIVATE" spark
+
+python -m ipykernel install
+
+"$CONDA" install -y tensorflow theano keras pytorch torchvision \
+                    numpy pandas scipy blaze patsy \
+                    networkx nltk scikit-learn statsmodels \
+                    matplotlib seaborn bokeh \
+                    ipython nbconvert nbformat \
+                    beautifulsoup4 boto boto3 requests \
+                    xlrd xlsxwriter xlwt \
+                    psycopg2 pyodbc sqlalchemy \
+                    six us tqdm sqlalchemy pandoc pandocfilters \
+                    nose sympy snappy cython jinja2 lxml pyyaml \
+                    pyspark
+
+source "$DEACTIVATE"
+EOF
 
---
+++ steps/python_bootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -xe
+
+# Install system packages
+sudo yum -y update
+sudo yum groups mark convert
+sudo yum groupinstall -y "Development Tools"
+sudo yum install -y ca-certificates unixODBC unixODBC-devel curl wget vim \
+                    libxml2-devel openssl-devel sqlite-devel python3-devel
+
+# Python packages for Spark
+ANACONDA_SCRIPT="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+
+INSTALL_DIR="/opt/conda"
+CONDA="$INSTALL_DIR/bin/conda"
+ACTIVATE="$INSTALL_DIR/bin/activate"
+DEACTIVATE="$INSTALL_DIR/bin/deactivate"
+
+wget "$ANACONDA_SCRIPT" -O ~/anaconda.sh
+sudo bash ~/anaconda.sh -b -p "$INSTALL_DIR"
+
+sudo "$CONDA" config --add channels conda-forge
+sudo "$CONDA" config --add channels pytorch
+
+"$CONDA" create -n spark anaconda python=3.6
+source "$ACTIVATE" spark
+
+sudo "$CONDA" install -y -n spark tensorflow theano keras pytorch torchvision \
+                               numpy pandas scipy blaze patsy \
+                               networkx nltk scikit-learn statsmodels \
+                               matplotlib seaborn bokeh \
+                               ipython nbconvert nbformat \
+                               beautifulsoup4 boto boto3 requests \
+                               xlrd xlsxwriter xlwt \
+                               psycopg2 pyodbc sqlalchemy \
+                               six us tqdm sqlalchemy pandoc pandocfilters \
+                               nose sympy snappy cython jinja2 lxml pyyaml \
+                               pyspark
+
+source "$DEACTIVATE"
+
