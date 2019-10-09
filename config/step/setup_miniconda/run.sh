b5461d2cd6ee5f84b914fdd2475fa2a6de39d657
William Brannon <will.brannon@gmail.com>
2019-10-09 14:09:15 -0400

    reorganize files

--- config/step/setup_miniconda/run.sh
+++ config/step/setup_miniconda/run.sh
@@ -3,6 +3,6 @@
 echo -e << EOF >> /etc/spark/conf/spark-env.sh
 \nexport PYSPARK_PYTHON="$HOME/conda/bin/python"
 export PYSPARK_DRIVER_PYTHON="$HOME/conda/bin/jupyter"
-export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1' # FIXME
 EOF
 
