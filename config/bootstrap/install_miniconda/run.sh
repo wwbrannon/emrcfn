63caaaa066336545dccab644bda7fc0e4bada13c
William Brannon <will.brannon@gmail.com>
2019-10-09 15:18:06 -0400

    pass args to config scripts

--- config/bootstrap/install_miniconda/run.sh
+++ config/bootstrap/install_miniconda/run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -xe
 
-wget -q "$1" -O "$HOME/miniconda.sh"
+# The environment file we want to install
+aws s3 cp "s3://$1/config/bootstrap/install_miniconda/environment.yml" .
+
+# Install conda
+wget -q "$2" -O "$HOME/miniconda.sh"
 bash "$HOME/miniconda.sh" -b -p "$HOME/conda"
+rm "$HOME/miniconda.sh" # clean up
 
 # the ".bash_local" bit is to avoid clashing with my dotfiles on master node
 echo -e "\nexport \"PATH=$HOME/conda/bin:$PATH\"" >> "$HOME/.bash_local"
 echo -e "\nsource \"$HOME/.bash_local\"" >> "$HOME/.bashrc"
 
 # install packages
-conda install -y ipython jupyter # FIXME
+conda env update -q -n base -f environment.yml
+rm -f environment.yml
 
