4f2d58fa728777fa57a32a9432de3b51e6205d87
William Brannon <will.brannon@gmail.com>
2019-10-09 13:57:58 -0400

    move files

--- steps/bin/step/miniconda_config.sh
+++ steps/bin/step/miniconda_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 echo -e << EOF >> /etc/spark/conf/spark-env.sh
-export PYSPARK_PYTHON="$HOME/conda/bin/python"
+\nexport PYSPARK_PYTHON="$HOME/conda/bin/python"
 export PYSPARK_DRIVER_PYTHON="$HOME/conda/bin/jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'
 EOF
