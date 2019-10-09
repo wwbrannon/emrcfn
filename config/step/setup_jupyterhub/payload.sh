5b5424e9acce96db271bfba6fb543b79f88e6e7f
William Brannon <will.brannon@gmail.com>
2019-10-09 18:07:08 -0400

    jupyterhub setup step

---
+++ config/step/setup_jupyterhub/payload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+# Install system packages in the container
+apt-get update && apt-get install -y build-essential
+
+# Ensure we have the same environment installed on the nodes
+# NOTE: assumes file copied to /tmp by other script
+conda env update -n base --file /tmp/environment.yml
+
--- config/step/setup_jupyterhub/run.sh
+++ config/step/setup_jupyterhub/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -xe
 
-# FIXME
+# The environment file we want to install
+aws s3 cp "s3://$1/config/bootstrap/install_miniconda/environment.yml" .
+aws s3 cp "s3://$1/config/bootstrap/install_miniconda/payload.sh" .
+
+sudo docker cp environment.yml jupyterhub:/tmp/
+sudo docker exec -i jupyterhub /bin/bash < payload.sh
+
+rm -f environment.yml payload.sh
 
