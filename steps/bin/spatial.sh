e6e9c7c470c478d0b11a88a0e7141d3299887508
William Brannon <will.brannon@gmail.com>
2019-10-09 13:47:02 -0400

    tweak script

--- steps/bin/spatial.sh
+++ steps/bin/spatial.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -xe
 
 # Download other files
-aws s3 cp --recursive "s3://$1/steps/" .
+aws s3 cp "s3://$1/steps/jar/bing-tile-hive-1.0.jar" .
+aws s3 cp "s3://$1/steps/jar/sql/spatial.sql" .
 
 # Install jars
 hadoop fs -rm -r -f hdfs:/spatial/
 hadoop fs -mkdir hdfs:/spatial/
-hadoop fs -copyFromLocal jar/bing-tile-hive-1.0.jar hdfs:/spatial/
+hadoop fs -copyFromLocal bing-tile-hive-1.0.jar hdfs:/spatial/
 
 # Run hive statements to create udfs
-hive -f sql/spatial.sql
+hive -f spatial.sql
+
+# Clean up
+rm -f bing-tile-hive-1.0.jar spatial.sql
 
