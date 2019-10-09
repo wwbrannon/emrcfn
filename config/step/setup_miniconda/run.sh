5b64d1222c2d7a9807d4ccd94504f75e6ed7492f
William Brannon <will.brannon@gmail.com>
2019-10-09 19:52:27 -0400

    bugfix

--- config/step/setup_miniconda/run.sh
+++ config/step/setup_miniconda/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-cat << EOF >> /etc/spark/conf/spark-env.sh
+cat << EOF | sudo tee -a /etc/spark/conf/spark-env.sh
 export PYSPARK_PYTHON="$HOME/conda/bin/python"
 export PYSPARK_DRIVER_PYTHON="$HOME/conda/bin/ipython"
 EOF
