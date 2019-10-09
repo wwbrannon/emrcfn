ebe806bc850629a8e346c1e82b1b8a3f47060401
William Brannon <will.brannon@gmail.com>
2019-10-09 14:30:11 -0400

    bugfix

--- config/step/setup_miniconda/run.sh
+++ config/step/setup_miniconda/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -xe
 
-echo -e << EOF >> /etc/spark/conf/spark-env.sh
-\nexport PYSPARK_PYTHON="$HOME/conda/bin/python"
+cat << EOF >> /etc/spark/conf/spark-env.sh
+export PYSPARK_PYTHON="$HOME/conda/bin/python"
 export PYSPARK_DRIVER_PYTHON="$HOME/conda/bin/jupyter"
-export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1' # FIXME
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=\$1'
 EOF
 
