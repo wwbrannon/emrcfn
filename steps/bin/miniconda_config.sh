fe6234e425fda7506d16cbc30e3602ca1f3058c3
William Brannon <will.brannon@gmail.com>
2019-10-09 13:23:26 -0400

    avoid hardcoding path

--- steps/bin/miniconda_config.sh
+++ steps/bin/miniconda_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -xe
 
 echo -e << EOF >> /etc/spark/conf/spark-env.sh
-export PYSPARK_PYTHON=/home/hadoop/conda/bin/python
-export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter
+export PYSPARK_PYTHON="$HOME/conda/bin/python"
+export PYSPARK_DRIVER_PYTHON="$HOME/conda/bin/jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'
 EOF
 
