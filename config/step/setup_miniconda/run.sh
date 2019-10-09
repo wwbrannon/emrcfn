bf11afa5ee802389b9b81ee724cb3c85693b1247
William Brannon <will.brannon@gmail.com>
2019-10-09 15:37:13 -0400

    update spark config variables

--- config/step/setup_miniconda/run.sh
+++ config/step/setup_miniconda/run.sh
@@ -2,7 +2,6 @@
 
 cat << EOF >> /etc/spark/conf/spark-env.sh
 export PYSPARK_PYTHON="$HOME/conda/bin/python"
-export PYSPARK_DRIVER_PYTHON="$HOME/conda/bin/jupyter"
-export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=\$1'
+export PYSPARK_DRIVER_PYTHON="$HOME/conda/bin/ipython"
 EOF
 
