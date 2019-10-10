20de6f44ae5d932ab1128271358d0ac0823ca332
William Brannon <will.brannon@gmail.com>
2019-10-09 21:59:15 -0400

    bugfix

--- config/step/setup_jupyterhub/run.sh
+++ config/step/setup_jupyterhub/run.sh
@@ -7,5 +7,10 @@ aws s3 cp "s3://$1/config/step/setup_jupyterhub/payload.sh" .
 sudo docker cp environment.yml jupyterhub:/tmp/
 sudo docker exec -i jupyterhub /bin/bash < payload.sh
 
+# Installing all this stuff does something bad to the
+# running jupyterhub process in the container, so let's
+# just restart it rather than worry about avoiding that
+sudo docker restart jupyterhub
+
 rm -f environment.yml payload.sh
 
