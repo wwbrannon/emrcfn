3ae44e6afd98b620505124d35be2f521f11c6dff
William Brannon <will.brannon@gmail.com>
2019-10-09 22:21:01 -0400

    maybe fix bug

--- config/step/setup_jupyterhub/payload.sh
+++ config/step/setup_jupyterhub/payload.sh
@@ -5,5 +5,6 @@ apt-get update && apt-get install -y build-essential
 
 # Ensure we have the same environment installed on the nodes
 # NOTE: assumes file copied to /tmp by other script
-conda env update -n base --file /tmp/environment.yml
+sudo -u jovyan conda env update -n base --file /tmp/environment.yml
+rm /tmp/environment.yml
 
